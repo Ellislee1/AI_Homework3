35d95e568d711f24614c6733d62790f78a926808
Ellis Thompson <>
2022-04-15 11:24:54 -0400

    player2 is always the competitor when there is a game key.

--- run.py
+++ run.py
@@ -3,18 +3,19 @@ from src.environment import Environment
 from src.player import Player
 from src.api import Api
 
-def main(player1, player2, shape, score):
+def main(player1, player2, shape, score, game_key = None):
     p1 = Player('X', player1)
-    p2 = Player('O', player2)
+
+    # If we have a game key then atleast one player should take actions through the API so it has to be a bot.
+    if game_key is not None:
+        p2 = Player('O', True, True)
+    else:
+        p2 = Player('O', player2)
+
 
     env = Environment(p1,p2, shape, to_win=score)
     env.play()
     # print(env.getState())
-
-def parseShape(string):
-    temp_string = string.replace('(','').replace(')','').replace(' ','').split(',')
-
-    return(int(temp_string[0]),int(temp_string[1]))
 
 def api():
     api = Api()
@@ -28,12 +29,14 @@ if __name__ == '__main__':
     parser.set_defaults(p2=False)
     parser.add_argument('-p1', '--player_1', action='store_true', dest='p1')
     parser.add_argument('-p2', '--player_2', action='store_true',dest='p2')
-
     parser.add_argument('-shape', '--shape', type=int, dest='shape', default=3)
     parser.add_argument('-s', '--score', type=int, dest='score', default=3)
+    parser.add_argument('-key', '--k', type=str, dest="game_key", default=None)
+
     args = parser.parse_args()
     shape = args.shape
     score = args.score
-    main(args.p1, args.p2, shape, score)
+    game_key = args.game_key
+    main(args.p1, args.p2, shape, score, game_key)
     # api()
 
